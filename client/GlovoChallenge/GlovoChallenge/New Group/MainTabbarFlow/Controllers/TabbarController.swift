1ec671fa0b9d5aff3a133a31f1124641e994cb5e
Ibrahim Kteish <ibrahim.kteish@cardinalhealth.com>
2018-07-23 13:03:06 +0300

    Implement tab bar controller and link it to the main storyboard

--- client/GlovoChallenge/GlovoChallenge/New Group/MainTabbarFlow/Controllers/TabbarController.swift
+++ client/GlovoChallenge/GlovoChallenge/New Group/MainTabbarFlow/Controllers/TabbarController.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+
+protocol TabBarViewOutput: BaseView {
+  var onViewDidLoad: PublishSubject<UINavigationController> { get }
+  var onHome: PublishSubject<UINavigationController> { get }
+  var onAbout: PublishSubject<UINavigationController> { get }
+}
+
+final class TabbarController: UITabBarController, UITabBarControllerDelegate, TabBarViewOutput {
+  let onViewDidLoad: PublishSubject<UINavigationController> = .init()
+  let onHome: PublishSubject<UINavigationController> = .init()
+  let onAbout: PublishSubject<UINavigationController> = .init()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.delegate = self
+    if let controller = customizableViewControllers?.first as? UINavigationController {
+      self.onViewDidLoad.onNext(controller)
+    }
+  }
+  
+  func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+    guard let controller = viewControllers?[selectedIndex] as? UINavigationController else { return }
+    
+    if selectedIndex == 0 {
+      self.onHome.onNext(controller)
+    } else if selectedIndex == 1 {
+      self.onAbout.onNext(controller)
+    }
+  }
+}
+
