dbce83680e8bdb96a50ccb3e8808101f04f07b28
Ibrahim Kteish <ibrahim.kteish@cardinalhealth.com>
2018-07-23 13:18:59 +0300

    Add home and about simple controllers

---
+++ client/GlovoChallenge/GlovoChallenge/Flow/About/Controllers/AboutController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutController.swift
+//  GlovoChallenge
+//
+//  Created by Ibrahim Kteish on 7/23/18.
+//  Copyright © 2018 Ibrahim Kteish. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol AboutViewOutput: BaseView {}
+
+final class AboutController: UIViewController, AboutViewOutput {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ client/GlovoChallenge/GlovoChallenge/Flow/Home/Controllers/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  GlovoChallenge
+//
+//  Created by Ibrahim Kteish on 7/23/18.
+//  Copyright © 2018 Ibrahim Kteish. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol HomeViewOutput: BaseView {}
+
+final class HomeController: UIViewController, HomeViewOutput {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
