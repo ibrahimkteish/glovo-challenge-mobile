8753d1072e322f44b0732977a2ec5df12834a50d
Ibrahim Kteish <ibrahim.kteish@cardinalhealth.com>
2018-07-23 11:13:38 +0300

    Implement base coordinator class

--- client/GlovoChallenge/GlovoChallenge/Application/BaseCoordinator.swift
+++ client/GlovoChallenge/GlovoChallenge/Application/BaseCoordinator.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+/// Base coornator class to which every coordinator is going to
+/// inherit form it. it has the minimum fuctionality that any coordinator should have.
+class BaseCoordinator: Coordinator {
+  // MARK: Properties
+  var childCoordinators: [Coordinator] = []
+  // MARK: Coordinator protocol implemantation
+  func start() {
+    fatalError("the class \(self) should override the start func")
+  }
+  
+  // MARK: Methods
+  
+  // Add only unique object
+  func addDependency(_ coordinator: Coordinator) {
+    for element in childCoordinators {
+      if element === coordinator { return } // swiftlint:disable:this for_where
+    }
+    childCoordinators.append(coordinator)
+  }
+  
+  func removeDependency(_ coordinator: Coordinator?) {
+    guard childCoordinators.isEmpty == false, let coordinator = coordinator else {
+      return
+    }
+    
+    for (index, element) in childCoordinators.enumerated() {
+      if element === coordinator { // swiftlint:disable:this for_where
+        childCoordinators.remove(at: index)
+        break
+      }
+    }
+  }
+}
+
