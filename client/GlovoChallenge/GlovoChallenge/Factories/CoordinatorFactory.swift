de0e7617c67065608f774d0f7bb02b5bf8a78fed
Ibrahim Kteish <ibrahim.kteish@cardinalhealth.com>
2018-07-23 12:18:12 +0300

    Add coordinator factory protocol

---
+++ client/GlovoChallenge/GlovoChallenge/Factories/CoordinatorFactory.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinatorFactory.swift
+//  GlovoChallenge
+//
+//  Created by Ibrahim Kteish on 7/23/18.
+//  Copyright © 2018 Ibrahim Kteish. All rights reserved.
+//
+
+import Foundation
+
+protocol CoordinatorFactory {
+  func makeTabBarCoordinator() -> (configurator: Coordinator, toPreesent: Presentable)
+}
