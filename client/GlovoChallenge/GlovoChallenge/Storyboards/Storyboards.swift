3b09818c3221e10447036defb3e23b97f98c0aa0
Ibrahim Kteish <ibrahim.kteish@cardinalhealth.com>
2018-07-23 11:49:30 +0300

    Add storyboards enum

---
+++ client/GlovoChallenge/GlovoChallenge/Storyboards/Storyboards.swift
@@ -0,0 +1,22 @@
+//
+//  Storyboards.swift
+//  GlovoChallenge
+//
+//  Created by Ibrahim Kteish on 7/23/18.
+//  Copyright © 2018 Ibrahim Kteish. All rights reserved.
+//
+
+import Foundation
+
+enum Storyboards: String {
+  case home = "Home"
+  case main = "Main"
+  
+  /*
+   In Real app we might have something like the following
+   case auth = "Auth"
+   case settings = "Settings"
+   case onBoarding = "OnBoarding"
+   case verification = "Verification"
+   */
+}
